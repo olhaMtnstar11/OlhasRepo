a4f3ca42c18ad23759c86e52c0e566067f9a8071
olhaMtnstar11 <olha@mtnstarcap.com>
2024-08-14 13:30:20 -0400

    footer default text

--- wp-content/themes/MYNEWTHEME/footer.php
+++ wp-content/themes/MYNEWTHEME/footer.php
@@ -5,6 +5,10 @@ wp_footer();
 <?php
 // Fetch the hero image URL from the Customizer
 $footer_text = get_theme_mod('footer_text');
+// Define your static text
+$static_text = 'default footer';
+// Check if $footer_text is not empty, otherwise use $static_text
+$text_to_display = !empty($footer_text) ? $footer_text : $static_text;
 
 ?>
 
@@ -13,7 +17,7 @@ $footer_text = get_theme_mod('footer_text');
 
 <footer class="footer text-center py-2 theme-bg-dark">
     <p class="copyright"><a href="">
-            <?php echo $footer_text; ?>
+            <?php echo $text_to_display; ?>
         </a></p>
     <?php
     dynamic_sidebar('footer-1');
