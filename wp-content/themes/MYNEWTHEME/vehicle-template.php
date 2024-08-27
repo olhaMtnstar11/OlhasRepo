44fce077b3b421078f5a17adc6fa841f592ca0d1
olhaMtnstar11 <olha@mtnstarcap.com>
2024-08-27 15:02:11 -0400

    vehicle template css

--- wp-content/themes/MYNEWTHEME/vehicle-template.php
+++ wp-content/themes/MYNEWTHEME/vehicle-template.php
@@ -1,33 +1,53 @@
 <?php
-/* Template Name: Vehicle template */
-get_header(); ?>
+/**
+ * Template Name: Vehicle Template
+ * Template Post Type: vehicle
+ */
+get_header();
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+// Query the posts
+$args = array(
+    'post_type'      => 'vehicle', // Custom post type
+    'posts_per_page' => -1,     // Display all posts
+);
+$posts_query = new WP_Query($args);
 
-            <?php
-            // Display content of the page
-            if ( have_posts() ) :
-                while ( have_posts() ) : the_post();
-                    the_content();
-                endwhile;
-            endif;
-
-            // Query for team members
-            $args = array(
-                'post_type' => 'vehicle',
-                'posts_per_page' => -1,
-
-            );
-
-
-
-
-
+if ($posts_query->have_posts()) :
+    ?>
+    <div class="vehicle-card-grid">
+        <?php
+        while ($posts_query->have_posts()) : $posts_query->the_post();
+            // Get ACF fields
+            $image_v = get_field('image_v');
+            $brand_v = get_field('brand_v');
+            $model_v = get_field('model_v');
+            $year_v = get_field('year_v');
+            $mileg_v = get_field('mileg_v');
+            $price_v = get_field('price_v');
+            $color_box_v = get_field('color_box_v');
 
             ?>
+            <div class="vehicle-card">
+                <?php if ($image_v) : ?>
+                    <div class="vehicle-card-image">
+                        <img src="<?php echo esc_url($image_v['url']); ?>" alt="<?php echo esc_attr($image_v['alt']); ?>" width="150" height="150">
+                    </div>
+                <?php endif; ?>
+                <div class="vehicle-card-content">
+                    <h2 class="vehicle-card-title"><?php echo esc_html($brand_v . ' ' . $model_v); ?></h2>
+                    <p><strong>Year:</strong> <?php echo esc_html($year_v); ?></p>
+                    <p><strong>Mileage:</strong> <?php echo esc_html($mileg_v); ?></p>
+                    <p><strong>Price:</strong> <?php echo esc_html($price_v); ?></p>
+                    <p><strong>Color:</strong> <?php echo esc_html($color_box_v); ?></p>
+                </div>
+            </div>
+        <?php
+        endwhile;
+        ?>
+    </div>
+    <?php
+    wp_reset_postdata(); // Restore original post data
+endif;
 
-        </main><!-- #main -->
-    </div><!-- #primary -->
+get_footer();
 
-<?php get_footer(); ?>
